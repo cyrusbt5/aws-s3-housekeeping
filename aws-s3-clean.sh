b1029b56dff30d05b7133410dda19c0011d77e4d
Robert Gurtler <cyrusbt5@users.noreply.github.com>
2015-08-05 22:49:17 +1000

    Updated aws-s3-clean.sh

--- aws-s3-clean.sh
+++ aws-s3-clean.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Usage: ./aws-s3-clean.sh "bucketname" "days"
+# Usage:   ./aws-s3-clean.sh "bucketname" "days"
+# Example: ./aws-s3-clean.sh "bucket-foo" "5"
 
 set -o errexit  # exit when a command fails.
 set -o pipefail # catch pipe fails in e.g. mysqldump | gzip
@@ -8,7 +9,7 @@ set -o nounset  # exit when your script tries to use undeclared variables
 # set internal field separator
 IFS=$'\n\t'
 
-# set default variables for arguments needed
+# set default variables for arguments
 bucket=${1:-}
 days=${2:-}
 
@@ -31,15 +32,17 @@ fi
 
 echo "Size of AWS S3 bucket before purge."
 ${s3cmd} du -r s3://${bucket} | awk '{printf "%.0f MB\n", $1/1024/1024 }'
+
 ${s3cmd} ls -r s3://${bucket} | grep -Ev "logs/" | while read -r line; do
   # timestamp=$(date -d"$(echo $line | awk {'print $1'})" '+%s')
   timestamp=$(date -j -f "%Y-%m-%d" "$(echo $line | awk {'print $1'})" '+%s')
-  if [[ $timestamp -lt $olderThan ]] ; then
+  if [[ $timestamp -lt $olderThan ]]; then
     fileName=$(echo $line | awk {'print $5'})
     if [[ $fileName != "" ]]; then
       ${s3cmd} rm "${fileName}" > /dev/null 2>&1
     fi
   fi
 done;
+
 echo "Size of AWS S3 bucket after purge."
 ${s3cmd} du -r s3://${bucket} | awk '{printf "%.0f MB\n", $1/1024/1024 }'
